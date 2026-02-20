5f899f5e88eb9e2458495104d3ec90699dc124c6
ndb4183 <33065039+ndb4183@users.noreply.github.com>
2026-02-20 16:03:29 -0600

    Update page.tsx

--- app/(login)/sign-in/page.tsx
+++ app/(login)/sign-in/page.tsx
@@ -1,10 +1,5 @@
-import { Suspense } from 'react';
-import { Login } from '../login';
+import { SignIn } from "@clerk/nextjs";
 
-export default function SignInPage() {
-  return (
-    <Suspense>
-      <Login mode="signin" />
-    </Suspense>
-  );
+export default function Page() {
+  return <SignIn />;
 }
